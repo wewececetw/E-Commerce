b82dce60b09bde2532b2f27d436ef30a4721adc6
楊洨貓 <wewececetw@gmail.com>
2022-12-15 13:13:42 +0800

    commit: 更改購物車內商品數量 fix: 前端註解刪除(會導致頁面顯示不了)

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -2,10 +2,28 @@
 
 namespace App\Http\Livewire;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Livewire\Component;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId)
+    {
+        //rowId是表示更新的购物车条目ID，第二个参数可以是数字用于表示更新后的数量，也可以是数组表示更新的属性
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId,$qty);
+    }
+
+    public function decreaseQuantity($rowId)
+    {
+        //rowId是表示更新的购物车条目ID，第二个参数可以是数字用于表示更新后的数量，也可以是数组表示更新的属性
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId, $qty);
+    }
+
+
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');
--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -1,4 +1,4 @@
-<!--main area-->
+
 	<main id="main" class="main-site left-sidebar">
 
 		<div class="container">
@@ -269,4 +269,4 @@
 		</div><!--end container-->
 
 	</main>
-	<!--main area-->
+
