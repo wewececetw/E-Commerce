d4875ee481fc5b8d84d482b2d498d34ff59aa06c
楊洨貓 <wewececetw@gmail.com>
2022-12-15 20:16:59 +0800

    commit: 商品詳情頁面數量開發

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -8,27 +8,51 @@ use Livewire\Component;
 
 class DetailsComponent extends Component
 {
+    public $product;
     public $slug;
-    
+    public $quantity = [];
+
     public function mount($slug){
+        
         $this->slug = $slug;
+        $this->product = Product::where('slug', $this->slug)->first();
+        
+        $this->quantity[$this->product->id] = 1;
+        
+    }
+
+    public function reduce($product_id)
+    {
+        if( $this->quantity[$product_id] > 1)
+        {
+            $this->quantity[$product_id] -= 1;
+        }
+       
+        
+    }
+    public function increase($product_id)
+    {
+        if ($this->quantity[$product_id] < 100) {
+            $this->quantity[$product_id] += 1;
+        }
+        
     }
 
     public function store($product_id, $product_name, $product_price)
     {
         //當更新一個 belongsTo 關聯時，你可以使用 associate 方法。此方法會設定外鍵至下層模型：
         // When a single item is added當單個item被添加
-        Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
+        Cart::add($product_id, $product_name, $this->quantity[$product_id], $product_price)->associate('App\Models\Product');
         session()->flash('success_message', '商品增加到購物車');
         return redirect()->route('product.cart');
     }
 
     public function render()
     {
-        $product = Product::where('slug', $this->slug)->first();
+        
         //inRandomOrder 隨機排列
         $popular_products = Product::inRandomOrder()->limit(4)->get();
-        $related_products = Product::where('category_id',$product->category_id)->inRandomOrder()->limit(5)->get();
-        return view('livewire.details-component',['product' => $product , 'popular_products' => $popular_products , 'related_products' => $related_products])->layout('layouts.base');
+        $related_products = Product::where('category_id', $this->product->category_id)->inRandomOrder()->limit(5)->get();
+        return view('livewire.details-component',['product' => $this->product , 'popular_products' => $popular_products , 'related_products' => $related_products])->layout('layouts.base');
     }
 }
--- resources/views/livewire/details-component.blade.php
+++ resources/views/livewire/details-component.blade.php
@@ -49,10 +49,10 @@
                             <div class="quantity">
                             	<span>Quantity:</span>
 								<div class="quantity-input">
-									<input type="text" name="product-quatity" value="1" data-max="120" pattern="[0-9]*" >
+									<input type="text" wire:model="quantity.{{ $product->id }}" name="product-quatity" data-max="120" pattern="[0-9]*" >
 									
-									<a class="btn btn-reduce" href="#"></a>
-									<a class="btn btn-increase" href="#"></a>
+									<a class="btn btn-reduce" href="" wire:click.prevent="reduce({{$product->id}})"></a>
+									<a class="btn btn-increase" href="" wire:click.prevent="increase({{$product->id}})"></a>
 								</div>
 							</div>
 							<div class="wrap-butons">
