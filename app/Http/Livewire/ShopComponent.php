f268638cdfc51e30af3f206dfd55a97339dc853e
楊洨貓 <wewececetw@gmail.com>
2022-12-16 13:00:11 +0800

    commit: 產品頁面排序和每頁產品數量控制(前端頁面商品排序&&頁數無js綁定後端)

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -8,6 +8,17 @@ use Livewire\Component;
 
 class ShopComponent extends Component
 {
+
+    public $sorting;
+    public $pagesize;
+
+    public function mount()
+    {
+        $this->sorting = "default";
+        $this->pagesize = 12;
+    }
+
+
     public function store($product_id,$product_name,$product_price)
     {
         //當更新一個 belongsTo 關聯時，你可以使用 associate 方法。此方法會設定外鍵至下層模型：
@@ -17,7 +28,22 @@ class ShopComponent extends Component
     }
     public function render()
     {
-        $products = Product::paginate(12);
+        if($this->sorting == "date")
+        {
+            $products = Product::orderBy('created_at','DESC')->paginate($this->pagesize);
+        }
+        else if($this->sorting == "price")
+        {
+            $products = Product::orderBy('regular_price', 'ASC')->paginate($this->pagesize);
+        } 
+        else if ($this->sorting == "price-desc")
+        {
+            $products = Product::orderBy('regular_price', 'DESC')->paginate($this->pagesize);
+        }
+        else
+        {
+            $products = Product::paginate($this->pagesize);
+        }
         return view('livewire.shop-component',[ 'products' => $products ])->layout('layouts.base');
     }
 }
