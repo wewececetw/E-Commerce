c6fcba528a555c1abe703c28f072284e63204c8e
楊洨貓 <wewececetw@gmail.com>
2022-12-16 14:13:15 +0800

    fix: 頁面換頁模組修復

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -5,9 +5,11 @@ namespace App\Http\Livewire;
 use App\Models\Product;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShopComponent extends Component
 {
+    use WithPagination;
 
     public $sorting;
     public $pagesize;
