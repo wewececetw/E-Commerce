27d428807b872f804c287c666fa657488eaef115
楊洨貓 <wewececetw@gmail.com>
2022-12-18 22:55:56 +0800

    test: git test

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/',HomeComponent::class)->name('home');
 
 Route::get('/shop',ShopComponent::class)->name('shop');
